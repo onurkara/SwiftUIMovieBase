ced9c12f5f35b3ac76afcbbee3350d7fde3b6a58
Onur Kara <onurkara92@gmail.com>
2020-10-03 20:44:10 +0300

    Add initial repo

---
+++ SwiftUIMovieBase/SwiftUIMovieBase/SwiftUIMovieBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMovieBaseApp.swift
+//  SwiftUIMovieBase
+//
+//  Created by 113060 on 3.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMovieBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
