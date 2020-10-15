7706d704b39d9cfdf6478a8bae874d0eb6e6468c
Onur Kara <onurkara92@gmail.com>
2020-10-15 21:53:44 +0300

    Add Lottie view and animation resource

---
+++ SwiftUIMovieBase/SwiftUIMovieBase/Views/LottieView.swift
@@ -0,0 +1,46 @@
+//
+//  LottieView.swift
+//  SwiftUIMovieBase
+
+import Lottie
+import SwiftUI
+
+struct LottiView: UIViewRepresentable {
+
+    typealias UIViewType = UIView
+
+    @State var animationNamed: String
+
+    func makeUIView(context: Context) -> UIView {
+        let containerView = UIView()
+
+        let animationView = AnimationView()
+        animationView.animation = Animation.named(animationNamed)
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.addSubview(animationView)
+        NSLayoutConstraint.activate(
+            [
+                animationView.widthAnchor.constraint(equalTo: containerView.widthAnchor),
+                animationView.heightAnchor.constraint(equalTo: containerView.heightAnchor)
+            ]
+        )
+
+        animationView.play()
+
+        return containerView
+    }
+
+    func updateUIView(_ uiView: UIView, context: Context) {
+
+    }
+}
+
+struct LottieView_Previews: PreviewProvider {
+
+    static var previews: some View {
+        LottiView(animationNamed: "loading_animation")
+    }
+}
