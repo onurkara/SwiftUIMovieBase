7235b9b9c0cd4da368dea71c2e56f831fec3a1bd
Onur Kara <onurkara92@gmail.com>
2020-11-22 23:26:34 +0300

    Add NavigationView to TopRatedMovieView

---
+++ SwiftUIMovieBase/SwiftUIMovieBase/Views/TopRatedMovieDetailView.swift
@@ -0,0 +1,13 @@
+//
+//  TopRatedMovieDetailView.swift
+//  SwiftUIMovieBase
+//
+
+import SwiftUI
+
+struct TopRatedMovieDetailView: View {
+
+    var body: some View {
+        Text("Soon")
+    }
+}
--- SwiftUIMovieBase/SwiftUIMovieBase/Views/TopRatedMovieView.swift
+++ SwiftUIMovieBase/SwiftUIMovieBase/Views/TopRatedMovieView.swift
@@ -7,24 +7,28 @@ import SwiftUI
 struct TopRatedMovieView: View {
 
     @ObservedObject var viewModel = TopRatedMoviesViewModel()
+    private let pageTitle = "Top Rated Movies"
 
     var body: some View {
-        VStack {
+        NavigationView {
             List(viewModel.topRatedMovies) { movie in
-                VStack(alignment: .leading) {
-                    Text(movie.title)
-                    Text(movie.releaseDate)
-                    if movie == viewModel.topRatedMovies.last {
-                        VStack(alignment: .center) {
-                            ProgressView().onAppear(perform: {
-                                viewModel.fetchTopRatedMovies()
-                            })
-                        }.frame(maxWidth: .infinity)
+                NavigationLink(destination: TopRatedMovieDetailView()) {
+                    VStack(alignment: .leading) {
+                        Text(movie.title)
+                        Text(movie.releaseDate)
                     }
                 }
-            }
-        }.onAppear(perform: {
-            viewModel.fetchTopRatedMovies()
-        })
+                if movie == viewModel.topRatedMovies.last {
+                    VStack(alignment: .center) {
+                      ProgressView().onAppear(perform: {
+                        viewModel.fetchTopRatedMovies()
+                        })
+                    }.frame(maxWidth: .infinity)
+                }
+            }.onAppear(perform: {
+                viewModel.fetchTopRatedMovies()
+            })
+            .navigationBarTitle(Text(pageTitle))
+        }
     }
 }
